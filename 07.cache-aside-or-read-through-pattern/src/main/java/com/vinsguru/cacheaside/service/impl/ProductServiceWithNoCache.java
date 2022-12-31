2ed5c1f12851e1514827ef93b28c404c2971a58a
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-12-31 11:31:42 +0530

    Update ProductServiceWithNoCache.java

--- 07.cache-aside-or-read-through-pattern/src/main/java/com/vinsguru/cacheaside/service/impl/ProductServiceWithNoCache.java
+++ 07.cache-aside-or-read-through-pattern/src/main/java/com/vinsguru/cacheaside/service/impl/ProductServiceWithNoCache.java
@@ -1,6 +1,7 @@
 package com.vinsguru.cacheaside.service.impl;
 
 import com.vinsguru.cacheaside.dto.ProductDto;
+import com.vinsguru.cacheaside.entity.Product;
 import com.vinsguru.cacheaside.repository.ProductRepository;
 import com.vinsguru.cacheaside.service.ProductService;
 import com.vinsguru.cacheaside.util.EntityDtoUtil;
@@ -19,17 +20,22 @@ public class ProductServiceWithNoCache implements ProductService {
 
     @Override
     public Mono<ProductDto> getProduct(Integer id) {
-        return productRepository.findById(id)
+        return this.productRepository.findById(id)
                 .map(EntityDtoUtil::toDto);
     }
 
     @Override
-    public Mono<Void> updateProduct(Integer id, Mono<ProductDto> productDto) {
-        return productRepository.findById(id)
-                .zipWith(productDto)
-                .doOnNext(t -> t.getT1().setQtyAvailable(t.getT1().getQtyAvailable()))
+    public Mono<Void> updateProduct(Integer id, Mono<ProductDto> mono) {
+        return this.productRepository.findById(id)
+                .zipWith(mono)
+                .doOnNext(t -> {
+                    Product p = t.getT1();
+                    p.setQtyAvailable(t.getT2().getQuantityAvailable());
+                    p.setDescription(t.getT2().getDescription());
+                    p.setPrice(t.getT2().getPrice());
+                })
                 .map(Tuple2::getT1)
-                .flatMap(p -> productRepository.save(p))
+                .flatMap(this.productRepository::save)
                 .then();
     }
 }
