c8d03203828b6baac43f6e2fbc023b2b1224b481
Prateek <54174687+javaHelper@users.noreply.github.com>
2023-09-07 15:22:57 +0530

    Add files via upload

---
+++ 15.bulk-head-pattern/src/main/java/com/example/controller/ProductAggregateController.java
@@ -0,0 +1,26 @@
+package com.example.controller;
+
+import com.example.dto.ProductAggregate;
+import com.example.service.ProductAggregatorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("sec10")
+public class ProductAggregateController {
+
+    @Autowired
+    private ProductAggregatorService service;
+
+    @GetMapping("product/{id}")
+    public Mono<ResponseEntity<ProductAggregate>> getProductAggregate(@PathVariable Integer id) {
+        return this.service.aggregate(id)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+}
---
+++ 15.bulk-head-pattern/src/main/java/com/example/dto/Review.java
@@ -0,0 +1,15 @@
+package com.example.dto;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class Review {
+
+    private Integer id;
+    private String user;
+    private Integer rating;
+    private String comment;
+
+}
